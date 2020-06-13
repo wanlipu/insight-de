c4165f6ad9aabb86a4a7e8df92c0a0b6ad2b89df
Wanli Pu <puwanli@gmail.com>
2020-06-13 13:53:23 -0500

    docs: add docstring

--- postgres/create_tables.py
+++ postgres/create_tables.py
@@ -4,6 +4,22 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def create_database(dbname, newdb, user, pwd, host_ip="127.0.0.1", port="5432"):
+    """
+    create a new database on postgres server on aws
+    
+    Args:
+        dbname: default database name to connect to server
+        newdb: name of the new database which is to be created
+        user: user name
+        pwd: user password
+        host_ip: ip for database server
+        port: port for database server
+    
+    Returns:
+        cur: cursor to the database
+        conn: connection to the database
+    """
+
     # connect to default database
     conn = psycopg2.connect(f"host={host_ip} port={port} dbname={dbname} user={user} password={pwd}")
     conn.set_session(autocommit=True)
@@ -25,32 +41,43 @@ def create_database(dbname, newdb, user, pwd, host_ip="127.0.0.1", port="5432"):
 
 
 def drop_tables(cur, conn):
+    """
+    drop tables in the database
+    
+    Args:
+        cur: cursor to the database
+        conn: connection to the database
+    
+    Returns:
+        None
+    """
+
     for query in drop_table_queries:
         cur.execute(query)
-        print(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    """
+    create tables in the database
+    
+    Args:
+        cur: cursor to the database
+        conn: connection to the database
+    
+    Returns:
+        None
+    """
+
     for query in create_table_queries:
         cur.execute(query)
-        print(query)
         conn.commit()
-
-
-def main():
-    cur, conn = create_database()
-    
-    drop_tables(cur, conn)
-    create_tables(cur, conn)
-
-    conn.close()
 
 
 if __name__ == "__main__":
     if len(sys.argv) == 7:
+        # parse command line inputs
         dbname, newdb, user, pwd, host_ip, port= sys.argv[1:]
-        print('hi')
 
         cur, conn = create_database(dbname, newdb, user, pwd, host_ip, port)
 
@@ -58,3 +85,5 @@ if __name__ == "__main__":
         create_tables(cur, conn)
 
         conn.close()
+    else:
+        print("Please check your inputs!")

72da4aba0f1df32545b6fdf534267b8e0b67a829
Wanli Pu <puwanli@gmail.com>
2020-06-18 11:12:04 -0500

    refactor: refactoring create_tables.py

--- postgres/create_tables.py
+++ postgres/create_tables.py
@@ -3,13 +3,12 @@ import psycopg2
 from sql_queries import create_table_queries, drop_table_queries
 
 
-def create_database(dbname, newdb, user, pwd, host_ip="127.0.0.1", port="5432"):
+def connect_database(newdb, user, pwd, host_ip="127.0.0.1", port="5432"):
     """
     create a new database on postgres server on aws
     
     Args:
-        dbname: default database name to connect to server
-        newdb: name of the new database which is to be created
+        dbname: name of the database which is to be created
         user: user name
         pwd: user password
         host_ip: ip for database server
@@ -20,17 +19,17 @@ def create_database(dbname, newdb, user, pwd, host_ip="127.0.0.1", port="5432"):
         conn: connection to the database
     """
 
-    # connect to default database
-    conn = psycopg2.connect(f"host={host_ip} port={port} dbname={dbname} user={user} password={pwd}")
-    conn.set_session(autocommit=True)
-    cur = conn.cursor()
+    # # connect to default database
+    # conn = psycopg2.connect(f"host={host_ip} port={port} dbname={dbname} user={user} password={pwd}")
+    # conn.set_session(autocommit=True)
+    # cur = conn.cursor()
     
-    # create database with UTF8 encoding
-    cur.execute(f"DROP DATABASE IF EXISTS {newdb}")
-    cur.execute(f"CREATE DATABASE {newdb} WITH ENCODING 'utf8' TEMPLATE template0")
+    # # create database with UTF8 encoding
+    # cur.execute(f"DROP DATABASE IF EXISTS {newdb}")
+    # cur.execute(f"CREATE DATABASE {newdb} WITH ENCODING 'utf8' TEMPLATE template0")
 
-    # close connection to default database
-    conn.close()    
+    # # close connection to default database
+    # conn.close()    
     
     # connect to insightde database
     conn = psycopg2.connect(f"host={host_ip} port={port} dbname={newdb} user={user} password={pwd}")
@@ -74,16 +73,49 @@ def create_tables(cur, conn):
         conn.commit()
 
 
+def query(cur):
+
+    cur.execute("SELECT count(*) FROM taxi;")
+    
+    for result in cur.fetchall():
+        print(f'There are {result[0]} rows in the table')
+
+    # cur.execute("SELECT COUNT(DISTINCT(trip_id)) FROM taxi;")
+    
+    # for result in cur.fetchall():
+    #     print(f'There are {result[0]} distinct trips')
+
+    cur.execute("SELECT COUNT(DISTINCT(taxi_id)) FROM taxi;")
+    
+    for result in cur.fetchall():
+        print(f'There are {result[0]} distinct taxi')
+
+    cur.execute("SELECT SUM(trip_mile) FROM taxi;")
+    
+    for result in cur.fetchall():
+        print(f'Sum of all trip distance is {result[0]}')
+
+    cur.execute("SELECT SUM(trip_sec) FROM taxi;")
+    
+    for result in cur.fetchall():
+        print(f'Sum of all trip time is {result[0]} secs')
+
+def close_connection(conn, cur):
+    cur.close()
+    conn.close()
+
 if __name__ == "__main__":
-    if len(sys.argv) == 7:
+    if len(sys.argv) == 6:
         # parse command line inputs
-        dbname, newdb, user, pwd, host_ip, port= sys.argv[1:]
+        dbname, user, pwd, host_ip, port= sys.argv[1:]
 
-        cur, conn = create_database(dbname, newdb, user, pwd, host_ip, port)
+        cur, conn = connect_database(dbname, user, pwd, host_ip, port)
 
-        drop_tables(cur, conn)
-        create_tables(cur, conn)
+        # drop_tables(cur, conn)
+        # create_tables(cur, conn)
 
-        conn.close()
+        query(cur)
+
+        close_connection(conn, cur)
     else:
         print("Please check your inputs!")

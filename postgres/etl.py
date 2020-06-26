fac9fc3512e0ca35c7eafdf4bd0db0ba9cf6a554
Wanli Pu <puwanli@gmail.com>
2020-06-25 22:41:02 -0500

    fix: fix order of inputs

--- postgres/etl.py
+++ postgres/etl.py
@@ -6,7 +6,7 @@ from helper import connect_database, process_file, table_stats
 if __name__ == "__main__":
     if len(sys.argv) == 8:
         # parse command line inputs
-        dbname, user, pwd, host_ip, port, filepath, table = sys.argv[1:]
+        dbname, user, pwd, host_ip, port, table, filepath = sys.argv[1:]
 
         # connect to database
         cur, conn = connect_database(dbname, user, pwd, host_ip, port)
